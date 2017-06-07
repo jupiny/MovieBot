fa00368aef5fa5bf71eee5f7780496d41d4cd56f
jupiny <tmdghks584@gmail.com>
2017-06-07 16:10:31 +0900

    Add get_naver_link_list_of_movies util fucntion

--- utils.py
+++ utils.py
@@ -1,15 +1,19 @@
 import json
 import random
 import re
+import os
 
 import requests
 
 
 MY_OPENAPI_SERVER = 'http://155.230.25.63:8080'
 MAX_DISPLAY_NUM = 5
+NAVER_MOVIE_API = 'https://openapi.naver.com/v1/search/movie.json'
+NAVER_CLIENT_ID = os.environ.get('NAVER_CLIENT_ID')
+NAVER_CLIENT_SECRET = os.environ.get('NAVER_CLIENT_SECRET')
 
 
-def get_title_of_movies(title='', genre=''):
+def get_title_list_of_movies(title='', genre=''):
     if title:
         response = requests.get(MY_OPENAPI_SERVER + '/movies/?title=' + title)
     elif genre:
@@ -23,3 +27,19 @@ def get_title_of_movies(title='', genre=''):
     year_parttern = r' \(\d{4}\)$'
     movie_title_list = [re.sub(year_parttern, '', movie['title']) for movie in json_data]
     return movie_title_list
+
+
+def get_naver_link_list_of_movies(title):
+    naver_link_list = []
+    if title:
+        response = requests.get(
+            NAVER_MOVIE_API+ '?query=' + title,
+            headers={
+                'X-Naver-Client-Id': NAVER_CLIENT_ID,
+                'X-Naver-Client-Secret': NAVER_CLIENT_SECRET,
+            }
+        )
+        json_data = json.loads(response.text)
+        naver_link = json_data['items'][0]['link']
+        naver_link_list.append(naver_link)
+    return naver_link_list
